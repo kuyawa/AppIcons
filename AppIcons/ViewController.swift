8de4e2476c38fc15dbe15ca44ec436e40af2d62f
kuyawa <haxapp@gmail.com>
2017-06-30 09:55:49 -0400

    New icon

--- AppIcons/ViewController.swift
+++ AppIcons/ViewController.swift
@@ -48,33 +48,22 @@ class ViewController: NSViewController {
     }
     
     func showMainImage(_ url: URL) {
-        image = NSImage(byReferencingFile: url.path)!
-        imageMain.image = image
+        self.image = NSImage(byReferencingFile: url.path)!
+        DispatchQueue.main.async { self.imageMain.image = self.image }
     }
     
     func generateImages() {
-        // Resize them all
-        images["016A"] = image.resize(  16,  16) // appicon_016pts_1X.png
-        images["016B"] = image.resize(  32,  32) // appicon_016pts_2X.png
-        images["032A"] = image.resize(  32,  32) // appicon_032pts_1X.png
-        images["032B"] = image.resize(  64,  64) // appicon_032pts_2X.png
-        images["128A"] = image.resize( 128, 128) // appicon_128pts_1X.png
-        images["128B"] = image.resize( 256, 256) // appicon_128pts_2X.png
-        images["256A"] = image.resize( 256, 256) // appicon_256pts_1X.png
-        images["256B"] = image.resize( 512, 512) // appicon_256pts_2X.png
-        images["512A"] = image.resize( 512, 512) // appicon_512pts_1X.png
-        images["512B"] = image.resize(1024,1024) // appicon_512pts_2X.png
-        // Save them all
-        DispatchQueue.main.async { self.image016A.image = self.images["016A"]; self.images["016A"]?.saveToDownloads("appicon_016pts_1X.png") }
-        DispatchQueue.main.async { self.image016B.image = self.images["016B"]; self.images["016B"]?.saveToDownloads("appicon_016pts_2X.png") }
-        DispatchQueue.main.async { self.image032A.image = self.images["032A"]; self.images["032A"]?.saveToDownloads("appicon_032pts_1X.png") }
-        DispatchQueue.main.async { self.image032B.image = self.images["032B"]; self.images["032B"]?.saveToDownloads("appicon_032pts_2X.png") }
-        DispatchQueue.main.async { self.image128A.image = self.images["128A"]; self.images["128A"]?.saveToDownloads("appicon_128pts_1X.png") }
-        DispatchQueue.main.async { self.image128B.image = self.images["128B"]; self.images["128B"]?.saveToDownloads("appicon_128pts_2X.png") }
-        DispatchQueue.main.async { self.image256A.image = self.images["256A"]; self.images["256A"]?.saveToDownloads("appicon_256pts_1X.png") }
-        DispatchQueue.main.async { self.image256B.image = self.images["256B"]; self.images["256B"]?.saveToDownloads("appicon_256pts_2X.png") }
-        DispatchQueue.main.async { self.image512A.image = self.images["512A"]; self.images["512A"]?.saveToDownloads("appicon_512pts_1X.png") }
-        DispatchQueue.main.async { self.image512B.image = self.images["512B"]; self.images["512B"]?.saveToDownloads("appicon_512pts_2X.png") }
+        // Resize and save
+        DispatchQueue.main.async { self.image016A.image = self.image.resize(  16,  16); self.image016A.image?.saveToDownloads("appicon_016pts_1X.png") }
+        DispatchQueue.main.async { self.image016B.image = self.image.resize(  32,  32); self.image016B.image?.saveToDownloads("appicon_016pts_2X.png") }
+        DispatchQueue.main.async { self.image032A.image = self.image.resize(  32,  32); self.image032A.image?.saveToDownloads("appicon_032pts_1X.png") }
+        DispatchQueue.main.async { self.image032B.image = self.image.resize(  64,  64); self.image032B.image?.saveToDownloads("appicon_032pts_2X.png") }
+        DispatchQueue.main.async { self.image128A.image = self.image.resize( 128, 128); self.image128A.image?.saveToDownloads("appicon_128pts_1X.png") }
+        DispatchQueue.main.async { self.image128B.image = self.image.resize( 256, 256); self.image128B.image?.saveToDownloads("appicon_128pts_2X.png") }
+        DispatchQueue.main.async { self.image256A.image = self.image.resize( 256, 256); self.image256A.image?.saveToDownloads("appicon_256pts_1X.png") }
+        DispatchQueue.main.async { self.image256B.image = self.image.resize( 512, 512); self.image256B.image?.saveToDownloads("appicon_256pts_2X.png") }
+        DispatchQueue.main.async { self.image512A.image = self.image.resize( 512, 512); self.image512A.image?.saveToDownloads("appicon_512pts_1X.png") }
+        DispatchQueue.main.async { self.image512B.image = self.image.resize(1024,1024); self.image512B.image?.saveToDownloads("appicon_512pts_2X.png") }
         
         buttonFolder.isHidden = false
     }
